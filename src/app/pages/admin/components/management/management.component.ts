cb6d6c8968c379af705b596c7d46179a1148080e
GiuseppeDeMasiAKKODIS <giuseppe.demasi@akkodis.com>
2023-04-17 14:24:11 +0200

    modified verification form

--- src/app/pages/admin/components/management/management.component.ts
+++ src/app/pages/admin/components/management/management.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { tap } from 'rxjs';
 import { AppService } from 'src/app/app.service';
@@ -22,12 +22,14 @@ export class ManagementComponent implements OnInit {
   public operationType: OperationType;
   public OperationType = OperationType;
 
+  public formSubmitAttempt: boolean;
+
   constructor(
     private appService: AppService,
     private route: ActivatedRoute,
     private router: Router,
-    private toast:ToastrService
-    ) {
+    private toast: ToastrService
+  ) {
 
     this.loading = false;
 
@@ -37,184 +39,147 @@ export class ManagementComponent implements OnInit {
     this.selectedRacket = this.router.getCurrentNavigation()?.extras.state as racketDto;
 
     this.racketForm = new FormGroup({
-      brand: new FormControl(null),
-      model: new FormControl(null),
+      brand: new FormControl(null, Validators.required),
+      model: new FormControl(null, Validators.required),
       oldPrice: new FormControl(null),
-      price: new FormControl(null),
+      price: new FormControl(null, Validators.required),
       sex: new FormControl(null),
-      mainColor: new FormControl(null),
+      mainColor: new FormControl(null, Validators.required),
       secondaryColor: new FormControl(null),
       profile: new FormControl(null),
       length: new FormControl(null),
       weight: new FormControl(null),
-      idProduct: new FormControl(null),
+      idProduct:new FormControl(null),
       typeGame: new FormControl(null),
-      typeProduct: new FormControl(null),
+      typeProduct:new FormControl(null),
       image: new FormControl(null),
       material: new FormControl(null),
       seniorityPlayer: new FormControl(null),
       shape: new FormControl(null),
-      age: new FormControl(null),
+      age:new FormControl(null),
       balance: new FormControl(null),
-      year: new FormControl(null),
+      year: new FormControl(null, Validators.required),
       urlProduct: new FormControl(null)
     });
+
+    this.formSubmitAttempt = false;
   }
 
   ngOnInit(): void {
-    this.selectedRacket && this.updateRacketForm()
+    this.selectedRacket && this.updateRacketForm();
+    console.log(this.racketForm,this.brand)
   }
 
   public get brand() {
-    return this.racketForm.get('brand')?.value;
+    return this.racketForm.get('brand');
   }
 
   public get oldPrice() {
-    return this.racketForm.get('oldPrice')?.value;
+    return this.racketForm.get('oldPrice');
   }
 
   public get price() {
-    return this.racketForm.get('price')?.value;
+    return this.racketForm.get('price');
   }
   public get sex() {
-    return this.racketForm.get('sex')?.value;
+    return this.racketForm.get('sex');
   }
   public get mainColor() {
-    return this.racketForm.get('mainColor')?.value;
+    return this.racketForm.get('mainColor');
   }
-  public get mosecondaryColordel() {
-    return this.racketForm.get('mosecondaryColordel')?.value;
+  public get secondaryColor() {
+    return this.racketForm.get('secondaryColor');
   }
   public get profile() {
-    return this.racketForm.get('profile')?.value;
+    return this.racketForm.get('profile');
   }
   public get length() {
-    return this.racketForm.get('length')?.value;
+    return this.racketForm.get('length');
   }
   public get weight() {
-    return this.racketForm.get('weight')?.value;
+    return this.racketForm.get('weight');
   }
   public get idProduct() {
-    return this.racketForm.get('idProduct')?.value;
+    return this.racketForm.get('idProduct');
   }
   public get typeGame() {
-    return this.racketForm.get('typeGame')?.value;
+    return this.racketForm.get('typeGame');
   }
   public get typeProduct() {
-    return this.racketForm.get('typeProduct')?.value;
+    return this.racketForm.get('typeProduct');
   }
   public get image() {
-    return this.racketForm.get('image')?.value;
+    return this.racketForm.get('image');
   }
   public get seniorityPlayer() {
-    return this.racketForm.get('seniorityPlayer')?.value;
+    return this.racketForm.get('seniorityPlayer');
   }
 
   public get shape() {
-    return this.racketForm.get('shape')?.value;
+    return this.racketForm.get('shape');
   }
   public get age() {
-    return this.racketForm.get('age')?.value;
+    return this.racketForm.get('age');
   }
   public get balance() {
-    return this.racketForm.get('balance')?.value;
+    return this.racketForm.get('balance');
   }
   public get year() {
-    return this.racketForm.get('year')?.value;
+    return this.racketForm.get('year');
   }
   public get urlProduct() {
-    return this.racketForm.get('urlProduct')?.value;
+    return this.racketForm.get('urlProduct');
   }
 
   public get model() {
-    return this.racketForm.get('model')?.value;
+    return this.racketForm.get('model');
   }
 
   public addRacket(): void {
+    this.formSubmitAttempt = true;
+    if (this.racketForm.valid) {
+      console.log('ok')
+      const racket = this.generateRacketObject();
+      this.appService.insertRacket(racket)
+        .pipe(
+          tap({
+            next: (res) => {
+              console.log("RESPONSE: ", res);
+              this.toast.success("Il prodotto è stato aggiunto con successo", "Nuovo Prodotto Inserito")
 
-    const racket: Partial<racketDto> = {
-      prezzo: this.price,
-      vecchioPrezzo: this.oldPrice,
-      marca: this.brand,
-      modello: this.model,
-      sesso: this.sex,
-      imageLink: this.image,
-      coloreUno: this.mainColor,
-      coloreDue: this.mosecondaryColordel,
-      profilo: this.profile,
-      lunghezza: this.length,
-      peso: this.weight,
-      numeroArticolo: this.idProduct,
-      puntoDiEquilibrio: 0,
-      tipoDiGioco: this.typeGame,
-      url: this.urlProduct,
-      tipoDiProdotto: this.typeProduct,
-      telaio: '',
-      nucleo: '',
-      livelloDiGioco: '',
-      forma: this.shape,
-      eta: this.age,
-      bilanciamento: this.balance,
-      anno: this.year
+            },
+            error: (err) => {
+              console.log(err);
+            },
+          })
+        ).subscribe();
     }
-    this.appService.insertRacket(racket)
-      .pipe(
-        tap({
-          next: (res) => {
-            console.log("RESPONSE: ", res);
-            this.toast.success("Il prodotto è stato aggiunto con successo","Nuovo Prodotto Inserito")
 
-          },
-          error: (err) => {
-            console.log(err);
-          },
-        })
-      ).subscribe();
   }
   public updateRacket(): void {
-    const racket: Partial<racketDto> = {
-      prezzo: this.price,
-      vecchioPrezzo: this.oldPrice,
-      marca: this.brand,
-      modello: this.model,
-      sesso: this.sex,
-      imageLink: this.image,
-      coloreUno: this.mainColor,
-      coloreDue: this.mosecondaryColordel,
-      profilo: this.profile,
-      lunghezza: this.length,
-      peso: this.weight,
-      numeroArticolo: this.idProduct,
-      puntoDiEquilibrio: 0,
-      tipoDiGioco: this.typeGame,
-      url: this.urlProduct,
-      tipoDiProdotto: this.typeProduct,
-      telaio: '',
-      nucleo: '',
-      livelloDiGioco: '',
-      forma: this.shape,
-      eta: this.age,
-      bilanciamento: this.balance,
-      anno: this.year,
-      //racketId: this.idProduct
-    }
-    const updatedRacket = { ...this.selectedRacket, ...racket }
-    this.appService
-      .updateRacket(updatedRacket)
-      .pipe(
-        tap({
-          next: (res) => {
-            this.loading = false;
-            this.toast.success("Il prodotto è stato modificato con successo","Prodotto Modificato")
+    this.formSubmitAttempt = true;
 
-          },
-          error: (err) => {
-            this.loading = false;
-            console.log(err);
-          },
-        })
-      )
-      .subscribe()
+    if (this.racketForm.valid) {
+
+      const racket = this.generateRacketObject()
+      const updatedRacket = { ...this.selectedRacket, ...racket }
+      this.appService
+        .updateRacket(updatedRacket)
+        .pipe(
+          tap({
+            next: (res) => {
+              this.loading = false;
+              this.toast.success("Il prodotto è stato modificato con successo", "Prodotto Modificato")
+
+            },
+            error: (err) => {
+              this.loading = false;
+              console.log(err);
+            },
+          })
+        )
+        .subscribe()
+    }
   }
 
   private cleanForm(): void {
@@ -227,7 +192,7 @@ export class ManagementComponent implements OnInit {
     this.racketForm.patchValue({ price: null });
     this.racketForm.patchValue({ sex: null });
     this.racketForm.patchValue({ mainColor: null });
-    this.racketForm.patchValue({ mosecondaryColordel: null });
+    this.racketForm.patchValue({ secondaryColor: null });
     this.racketForm.patchValue({ profile: null });
     this.racketForm.patchValue({ length: null });
     this.racketForm.patchValue({ weight: null });
@@ -244,7 +209,7 @@ export class ManagementComponent implements OnInit {
   }
 
   public updateRacketForm(): void {
-    const  racket = this.selectedRacket
+    const racket = this.selectedRacket
 
     this.racketForm.patchValue({ brand: racket.marca });
     this.racketForm.patchValue({ model: racket.modello });
@@ -252,7 +217,7 @@ export class ManagementComponent implements OnInit {
     this.racketForm.patchValue({ price: racket.prezzo });
     this.racketForm.patchValue({ sex: racket.sesso });
     this.racketForm.patchValue({ mainColor: racket.coloreUno });
-    this.racketForm.patchValue({ mosecondaryColordel: racket.coloreDue });
+    this.racketForm.patchValue({ secondaryColor: racket.coloreDue });
     this.racketForm.patchValue({ profile: racket.profilo });
     this.racketForm.patchValue({ length: racket.lunghezza });
     this.racketForm.patchValue({ weight: racket.peso });
@@ -267,5 +232,40 @@ export class ManagementComponent implements OnInit {
     this.racketForm.patchValue({ year: racket.anno });
     this.racketForm.patchValue({ urlProduct: racket.url });
 
+  }
+
+  public fieldIsInvalid(fieldArg:string): any {
+    const field = this.racketForm.get(fieldArg)
+    return (field?.invalid && field?.touched) || (field?.invalid && this.formSubmitAttempt)
+    //(userJobHS.status=='INVALID'  && userJobHS.touched) || (userJobHS.status=='INVALID' && trySubmit)"
+  }
+
+  private generateRacketObject(): Partial<racketDto> {
+    const racket: Partial<racketDto> = {
+      prezzo: this.price?.value,
+      vecchioPrezzo: this.oldPrice?.value,
+      marca: this.brand?.value,
+      modello: this.model?.value,
+      sesso: this.sex?.value,
+      imageLink: this.image?.value,
+      coloreUno: this.mainColor?.value,
+      coloreDue: this.secondaryColor?.value,
+      profilo: this.profile?.value,
+      lunghezza: this.length?.value,
+      peso: this.weight?.value,
+      numeroArticolo: this.idProduct?.value,
+      puntoDiEquilibrio: 0,
+      tipoDiGioco: this.typeGame?.value,
+      url: this.urlProduct?.value,
+      tipoDiProdotto: this.typeProduct?.value,
+      telaio: '',
+      nucleo: '',
+      livelloDiGioco: '',
+      forma: this.shape?.value,
+      eta: this.age?.value,
+      bilanciamento: this.balance?.value,
+      anno: this.year?.value,
+    }
+    return racket;
   }
 }
